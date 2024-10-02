b132e90d0077e8c41e9aec3b56ec24c6a999de6d
Deshp666 <motvey74@mail.ru>
2024-10-02 13:57:31 +0500

    module 6

---
+++ 07_module/1/index.js
@@ -0,0 +1,11 @@
+function find(arr, search) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === search){
+            return i
+        }
+        
+    }
+}
+const a = 123
+
+console.log(a);
